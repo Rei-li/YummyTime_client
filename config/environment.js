81b956f6d7b418a992c9a93fed6431c05faecbb8
Rei-li <rei-li@mail.ru>
2016-05-17 00:14:58 +0300

    change dev host

--- config/environment.js
+++ config/environment.js
@@ -4,7 +4,7 @@ module.exports = function(environment) {
   const ENV = {
     modulePrefix: 'client',
     environment,
-    host: 'https://my-yummytime-server.herokuapp.com',
+    host: 'http://localhost:3000',
     baseURL: '/',
     locationType: 'auto',
     namespace: '',
@@ -68,7 +68,6 @@ module.exports = function(environment) {
   if (environment === 'production') {
     ENV.host = 'https://my-yummytime-server.herokuapp.com';
     ENV.namespace = '';
-    // ENV['ember-simple-auth-token'].serverTokenEndpoint = `${ENV.namespace}/auth/token`;
     ENV.torii.providers['google-oauth2'].redirectUri = ENV.host;
 
     // eslint-disable-next-line no-console
