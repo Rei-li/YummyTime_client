f91a9a2ae47a76c3abf0d38785ac88feef1c62f7
Rei-li <rei-li@mail.ru>
2016-05-17 14:14:10 +0300

    fix host

--- config/environment.js
+++ config/environment.js
@@ -67,7 +67,7 @@ module.exports = function(environment) {
 
   if (environment === 'production') {
     // ENV.host = 'https://my-yummytime-server.herokuapp.com';
-    ENV.host = 'http://yummytimeserver.azurewebsites.net/';
+    ENV.host = 'http://yummytimeserver.azurewebsites.net';
     ENV.namespace = '';
     ENV['ember-simple-auth-token'].serverTokenEndpoint = `${ENV.host}/auth/token`;
     ENV.torii.providers['google-oauth2'].redirectUri = ENV.host;
