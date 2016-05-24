bcbd82421b31d53a027d9aa0ff2f4e832bf0823c
Rei-li <rei-li@mail.ru>
2016-05-24 15:16:06 +0300

    add logical deletion

--- app/models/portion-product.js
+++ app/models/portion-product.js
@@ -2,5 +2,6 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   quantity: DS.attr('number'),
-  product: DS.belongsTo('product')
+  product: DS.belongsTo('product'),
+  deleted: DS.attr('boolean')
 });
