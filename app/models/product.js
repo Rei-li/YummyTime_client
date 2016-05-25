14b10e5e60a4d9ed97ef7dc3eb11d1e312fd52c3
Rei-li <rei-li@mail.ru>
2016-05-26 02:59:11 +0300

    update product model

--- app/models/product.js
+++ app/models/product.js
@@ -5,5 +5,8 @@ export default DS.Model.extend({
   description: DS.attr('string'),
   price: DS.attr('number'),
   imageUrl: DS.attr('string'),
-  vendor: DS.belongsTo('vendor')
+  vendor: DS.belongsTo('vendor'),
+  category: DS.attr('string'),
+  vendorId: DS.attr('string'),
+  deletedByVendor: DS.attr('boolean')
 });
